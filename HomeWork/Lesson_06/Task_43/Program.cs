aa6472878d7d945189bc5e3c28c11abb242dcc79
aleksey <alekseymalinovskiy@gmail.com>
2023-01-28 12:54:36 +0300

    Lesson_6

---
+++ HomeWork/Lesson_06/Task_43/Program.cs
@@ -0,0 +1,58 @@
+﻿//Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, 
+//заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+//b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+
+Console.Clear();
+System.Console.Write("Введите b1: ");
+int b1 = System.Convert.ToInt32(System.Console.ReadLine());
+System.Console.Write("Введите k1: ");
+int k1 = System.Convert.ToInt32(System.Console.ReadLine());
+System.Console.Write("Введите b2: ");
+int b2 = System.Convert.ToInt32(System.Console.ReadLine());
+System.Console.Write("Введите k2: ");
+int k2 = System.Convert.ToInt32(System.Console.ReadLine());
+
+ShowResult(b1, b2, k1, k2);
+
+static void ShowResult(double b1, double b2, double k1, double k2)
+{
+    string result;
+    if (Parallel(k1, k2)) result = "линии параллельны";
+    else
+    {
+        double x = SearchX(b1, b2, k1, k2);
+        double y = SearchY(b1, b2, k1, k2);
+        result = x + "; " + y;
+    }
+
+    if (Merge(b1, b2, k1, k2)) result = "линии совпадают";
+    if (Perpendicular(k1, k2)) result = "линии перпендикулярны";
+
+    System.Console.Write($"b1 = {b1}, k1 = {k1}, b2 = {b2}, k2 = {k2} -> ({result})");
+}
+
+static bool Parallel(double k1, double k2)
+{
+    return k1 == k2;
+}
+
+static bool Merge(double b1, double b2, double k1, double k2)
+{
+    return k1 == k2 && b1 == b2 && k1 == b1;
+}
+
+static bool Perpendicular(double k1, double k2)
+{
+    return k1 * k2 == -1;
+}
+
+static double SearchX(double b1, double b2, double k1, double k2)
+{
+    return (b2 - b1) / (k1 - k2);
+}
+
+static double SearchY(double b1, double b2, double k1, double k2)
+{
+    return k1 * ((b2 - b1) / (k1 - k2)) + b1;
+}
---
+++ HomeWork/Lesson_07/Task_52/Program.cs
@@ -0,0 +1,64 @@
+﻿// Задача 52: Задайте двумерный массив из целых чисел. Найдите среднее 
+// арифметическое элементов в каждом столбце.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+Console.Clear();
+
+Console.Write("Введите количество строк массива:\t");
+int rows = int.Parse(Console.ReadLine());
+
+Console.Write("Введите количество столбцов массива:\t");
+int columns = int.Parse(Console.ReadLine());
+
+
+int[,] array = GetArray(rows, columns, 0, 9);
+PrintArray(array);
+Console.WriteLine();
+
+
+int[,] GetArray(int rows, int columns, int minValue, int maxValue)
+{
+    int[,] result = new int[rows, columns];
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            result[i, j] = new Random().Next(minValue, maxValue + 1);
+        }
+    }
+    return result;
+}
+
+void PrintArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+            Console.Write($"{matr[i, j]} ");
+        Console.WriteLine();
+    }
+}
+
+void MeanColumns(int[,] array)
+{
+    double sum;
+    double result;
+
+    for (int i = 0; i < array.GetLength(1); i++)
+    {
+        sum = 0;
+        for (int j = 0; j < array.GetLength(0); j++)
+        {
+            sum += array[j, i];
+        }
+        result = sum / array.GetLength(0);
+        Console.WriteLine(string.Format("{0:f1}", result));
+    }
+}
+
+MeanColumns(array);
